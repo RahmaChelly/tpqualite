e416977b18c355ac4234d8e8213c964a99ad4a15
Ghaith <ghaithtroudi@gmail.com>
2017-11-25 21:47:58 +0100

    test

---
+++ src/test/java/entities/ChambreTest.java
@@ -0,0 +1,49 @@
+package entities;
+
+import junit.framework.TestCase;
+
+public class ChambreTest extends TestCase {
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void tearDown() throws Exception {
+    }
+
+    public void testToString() throws Exception {
+    }
+
+    public void testReserver() throws Exception {
+    }D
+
+    public void testAnnulerReservation() throws Exception {
+    }
+
+    public void testAjouterResevation() throws Exception {
+    }
+
+    public void testGetChambreId() throws Exception {
+    }
+
+    public void testSetChambreId() throws Exception {
+    }
+
+    public void testGetTypeChambre() throws Exception {
+    }
+
+    public void testSetTypeChambre() throws Exception {
+    }
+
+    public void testGetPrix() throws Exception {
+    }
+
+    public void testSetPrix() throws Exception {
+    }
+
+    public void testGetEtat() throws Exception {
+    }
+
+    public void testSetEtat() throws Exception {
+    }
+
+}
